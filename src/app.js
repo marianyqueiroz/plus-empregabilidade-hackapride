40af79046279ba2dbedefed0b03b640ad28aec1d
marianyqueiroz <marianyqs@gmail.com>
2020-11-20 16:31:38 -0300

    Add app and server

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const index = require("./routes/index");
+const empresas = require("./routes/empresasRoute");
+const colaboradores = require("./routes/colaboradoresRoute");
+const vagas = require("./routes/vagasRoute");
+
+mongoose.connect("mongodb://localhost:27017/plus",  { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", function (){
+    console.log("Successful connection!");
+});
+
+app.use(bodyParser.json());
+app.use(function(res, req, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Request-With, Content-Type, Accept"
+    );
+});
+app.use("/", index);
+app.use("/empresas", require("./routes/empresasRoute"));
+app.use("/colaboradores", require("./routes/colaboradoresRoute"));
+app.use("/vagas", require("./routes/vagasRoute"));
+
+module.exports = app;
